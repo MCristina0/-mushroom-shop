f58ff5ff09d9374cbe890de291e19266d974197a
MCristina0 <juagadorade2018@gmail.com>
2024-10-25 11:18:30 -0500

    creacionde odjetos

---
+++ app.js
@@ -0,0 +1,140 @@
+const products =[{
+id: 1,
+name: " Agarico",
+price: 10.99,
+img:'img/Agáricos.jpeg'
+},
+{
+id:2,
+name: "apagador ",
+price: 9.99  ,
+img:'img/Apagador.jpeg'
+}, {
+id:3,
+name: "armillaria",
+price: 9.99,
+stack:100,
+img:'img/Armillaria.jpeg'
+},
+ {
+id:4,
+name: "Buletus",
+price: 9.99,
+stack:100,
+img:'img/Boletus.jpeg'
+}
+, {
+id:5,
+name: "cahmpinones comun",
+price: 9.99,
+stack:100,
+img:'img/Champiñón común.jpeg'
+}
+, {
+id:6,
+name: "cordyceps",
+price: 9.99,
+stack:100,
+img:'img/Cordyceps.jpeg'
+}
+, {
+id:7,
+name: "cucumelo",
+price: 9.99,
+stack:100,
+img:'img/Cucumelo.jpeg'
+}
+, {
+id:8,
+name: "falsa ornja",
+price: 9.99,
+stack:100,
+img:'img/Falsa oronja.jpeg'
+}
+, {
+id:9,
+name: "flammulina",
+price: 9.99,
+stack:100,
+img:'img/Flammulina velutipes.jpeg'
+}
+, {
+id:10,
+name: "fughi porcini",
+price: 9.99,
+stack:100,
+img:'img/Funghi porcini.jpeg'
+}
+, {
+id:11,
+name: "Hongo de la muete",
+price: 9.99,
+stack:100,
+img:'img/Hongo de la muerte.jpeg'
+}
+, {
+id:12,
+name: "Hongo delicioso",
+price: 9.99,
+stack:100,
+img:'img/Hongo delicioso.jpeg'
+}
+, {
+id:13,
+name: "Hongo ostra",
+price: 9.99,
+stack:100,
+img:'img/Hongo ostra.jpeg'
+}
+, {
+id:14,
+name: "Hongo reoshi",
+price: 9.99,
+stack:100,
+img:'img/Hongo reishi.jpeg'
+}
+, {
+id:15,
+name: "melena de leon",
+price: 9.99,
+stack:100,
+img:'img/Melena de león.jpeg'
+}
+, {
+id:16,
+name: "Rebozuelo",
+price: 9.99,
+stack:100,
+img:'img/Rebozuelo.jpeg'
+}
+, {
+id:17,
+name: "seta de cardo",
+price: 9.99,
+stack:100,
+img:'img/Seta de cardo.jpeg'
+}
+, {
+id:18,
+name: "shiitake",
+price: 9.99,
+stack:100,
+img:'img/Shiitake.jpeg'
+}
+, {
+id:19,
+name: "tricholoma matsytake",
+price: 9.99,
+stack:100,
+img:'img/Tricholoma matsutake.jpeg'
+},
+{
+id:19,
+name: "trufas",
+price: 9.99,
+stack:100,
+img:'img/Trufas.jpeg'
+},
+
+
+] 
